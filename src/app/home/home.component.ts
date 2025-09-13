98fe03f4765ac146d36100e7aa628ab3e191f6d6
Murray James <muzware@gmail.com>
2025-09-13 17:05:17 +1000

    Add home.component.ts

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <h1>Welcome to Angular Auth Boilerplate!</h1>
+    <p>This is a protected home page. You are logged in.</p>
+  `
+})
+export class HomeComponent {}
